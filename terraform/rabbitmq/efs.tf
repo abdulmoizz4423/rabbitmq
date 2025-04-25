a70d42df77e3321d5d26277cc231ea41ba2fc234
mibrahimarfath826 <ibrahimarfath826@gmail.com>
2025-04-24 21:11:07 -0600

    changes added

--- terraform/rabbitmq/efs.tf
+++ terraform/rabbitmq/efs.tf
@@ -7,3 +7,9 @@ resource "aws_efs_file_system" "rabbitmq-fs" {
     Name = "rabbitmq-efs"
   }
 }
+
+resource "aws_efs_mount_target" "rabbitmq_mt" {
+
+  file_system_id = aws_efs_file_system.rabbitmq-fs.id
+  subnet_id      = data.aws_subnet.main.id
+}
